ed8f7a72619e73f4f48e2c783cd7fec8dd94f2cd
Kaif Chaudhary <kaifchgee@gmail.com>
2026-03-15 17:04:48 +0500

    Add files via upload

---
+++ kaiflibs/developer.js
@@ -0,0 +1,17 @@
+// Developer Configuration
+// These are the special VIP users who get special treatment across all bots.
+
+module.exports = {
+    // List of Developer Numbers (JIDs will be normalized)
+    developerNumbers: [
+        '923298634113',  // Developer 1
+        '923453684061',  // Developer 2 (Owner)
+        '923466859436'   // Developer 3
+    ],
+
+    // The Global Group where these developers hang out
+    globalGroupJid: '120363424931027427@g.us', // Global Group JID
+
+    // Reaction Emoji
+    reactionEmoji: '👨‍💻'
+};
